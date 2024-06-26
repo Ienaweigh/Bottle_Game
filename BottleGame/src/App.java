8393fc383004835cec479f35ce5adde7253cb5eb
Ienaweigh <87295637+Ienaweigh@users.noreply.github.com>
2024-06-25 21:37:46 -0700

    Update App.java
    created frame

--- BottleGame/src/App.java
+++ BottleGame/src/App.java
@@ -1,3 +1,7 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         BottleGame bottleGame = new BottleGame();
