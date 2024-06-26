76471ba53172a9fbe12361a146ba11dc8c4ec1ac
Ienaweigh <87295637+Ienaweigh@users.noreply.github.com>
2024-06-25 21:38:10 -0700

    Update BottleGame.java
    added frame

--- BottleGame/src/BottleGame.java
+++ BottleGame/src/BottleGame.java
@@ -1,3 +1,20 @@
-public class BottleGame {
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random.*;
+import javax.swing.*;
 
+public class BottleGame {
+    int screenWidth = 700;
+    int screenHeight = 750; 
+
+    JFrame frame = new JFrame("Bottle Game");
+
+    BottleGame(){
+        frame.setVisible(true);
+        frame.setSize(screenWidth, screenHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+    }
 }
